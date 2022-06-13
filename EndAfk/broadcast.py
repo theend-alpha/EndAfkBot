ffe6c6f9ec5aadef924306f81385a74d4c6c79bd
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 15:43:52 +0530

    Update broadcast.py

--- EndAfk/broadcast.py
+++ EndAfk/broadcast.py
@@ -21,7 +21,7 @@ async def total_users(_, message: Message):
         users_m = (await _.get_users(omfoo)).mention
         m_plus_id = f"{users_m} ({omfoo})"
         msg = """"""
-        msg += m_plus_id + "\n"
+        msg +="• " + m_plus_id + "\n"
     afk_users_msg = f"""• Afk users: \n\n {msg} \n• Count: {len(afk_users)}"""
     await message.reply_text(afk_users_msg)
 
