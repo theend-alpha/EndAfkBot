00fdd9d9464e5ae5045effbc31b9d6f0c18712d6
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-13 13:40:59 +0530

    Update broadcast.py

--- EndAfk/broadcast.py
+++ EndAfk/broadcast.py
@@ -18,9 +18,9 @@ async def total_users(_, message: Message):
     except Exception as e:
         return await message.reply_text(f"**Error:-** {e}")
     for omfoo in afk_users:
-        omfooo = str(omfoo)
+        users_m = (await _.get_users(omfoo)).mention
         msg = """"""
-        msg += "\n" + omfooo
+        msg += "\n" + users_m
     afk_users_msg = f""" Afk users: \n\n {msg} \n\n Count: {len(afk_users)}"""
     await message.reply_text(afk_users_msg)
 
