70a36f4fa714f72af963ec2b4bf1991f9bf54451
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-27 00:55:17 +0530

    Update __init__.py

--- EndAfk/AlphaDB/__init__.py
+++ EndAfk/AlphaDB/__init__.py
@@ -1,2 +1,21 @@
 from .chats import *
 from .users import *
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+import os
+
+DATABASE_URL = os.environ.get('DATABASE_URL', None)
+
+DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
+
+
+def start() -> scoped_session:
+    engine = create_engine(DATABASE_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
